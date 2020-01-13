a2c6833ddd760075e828cdc14ae05b34a6a557a9
mchennupati <m.chennupati@gmail.com>
2020-01-13 22:01:27 +0100

    modified code to connect to mongodb atlas

--- app.js
+++ app.js
@@ -1,44 +1,72 @@
-var MongoClient = require('mongodb').MongoClient
-var axios = require('axios')
+let axios = require("axios");
+let MongoClient = require("mongodb").MongoClient;
+let mongoParams = { useNewUrlParser: true, useUnifiedTopology: true };
+// useUnifiedTopology: true
 
-let placeHolder = {"name": "schenn",
-                    "value" : "Play Whatever"
-                    }
-let mongoUrl = 'mmongodb+srv://user:password@cluster0-hnc4i.azure.mongodb.net/test?retryWrites=true&w=majority'
-let params = 
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }
+let dateFns = require("date-fns");
 
-var db_name = 'test'
+let t = dateFns.format(new Date(), "yyyy-MM-dd");
 
-async function saveData(searchQuery){
+let tMonth = dateFns.format(dateFns.addMonths(new Date(), 1), "yyyy-MM-dd");
+let tWeek = dateFns.format(dateFns.addWeeks(new Date(), 1), "yyyy-MM-dd");
 
-    let client = new MongoClient(mongoUrl, params)
+let baseUrl = "https://api.stlouisfed.org/fred";
 
-    try{
-        client.connect( (err,res) => {
-            if (err) throw err
-            let db = client.db(db_name)
-            db.collection("macroData").insertOne(placeHolder)
-            console.log("connected successfully")
+let apiKey = 'b5271dff85f7f82f8de9b29aa8eb3c40'
+let mongoUrl = 'mongodb+srv://mchennupati:Tucher1@cluster0-hnc4i.azure.mongodb.net/test'
 
-            var query = {"value":"Whatever"}
-            db.collection("macroData").find(query).toArray( (err, result)=>{
-                if (err) console.log(err)
-                console.log(result)
-            })
-        })
-    }
-    catch (err) {
-        console.log(err.stack)
-    }
+function builder(baseUrl, apiKey, start, end) {
+  let futureDate = "&include_release_dates_with_no_data=true";
 
+  let url =
+    baseUrl +
+    "/releases" +
+    "/dates?" +
+    "api_key=" +
+    apiKey +
+    "&realtime_start=" +
+    start +
+    "&realtime_end=" +
+    end +
+    futureDate +
+    "&file_type=json";
+  return url;
 }
+// a test json file to see if connecting to db works.
+let d = {
+  release_id: 440,
+  release_name: "Coinbase Index",
+  release_last_updated: "2020-01-13 06:00:02-06",
+  date: "2020-01-20"
+};
 
-saveData()
+let newUrl = builder(baseUrl, apiKey, t, tWeek);
+console.log(newUrl);
+// Get Data from Fred
+let client = new MongoClient(mongoUrl, mongoParams);
 
+client.connect(err => {
+  if (err) {
+    console.log(err.message);
+    throw new Error("failed to connect");
+  }
 
+  let datab = client.db("fred");
+  console.log("db connected");
 
-
+  try {
+    axios.get(newUrl).then(res => {
+      try {
+        datab
+          .collection("macroData")
+          .insertOne({ dates: res.data.release_dates });
+        console.log("insert succeeded");
+      } catch (err) {
+        console.log("insert failed");
+        console.log(err.message);
+      }
+    });
+  } catch (err) {
+    throw Error("axios get did not work");
+  }
+});
