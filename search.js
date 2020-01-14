ed54423c741b8b742226bf8b5c9a75308bb9f170
mchennupati <m.chennupati@gmail.com>
2020-01-14 22:22:57 +0100

    search wrapped in a function

--- search.js
+++ search.js
@@ -1,36 +1,46 @@
 let MongoClient = require('mongodb').MongoClient
 let db_name = 'fred'
 let collection_name = 'macroData'
-let mongoUrl = "your url here"
+let mongoUrl = 'mongo url here'
 let mongoParams = {useNewUrlParser: true, useUnifiedTopology: true}
 
 
 let client = new MongoClient(mongoUrl,mongoParams)
 let result
 
-client.connect(err => {
+function searchCal(term, url) {
+  let result 
+  let client = new MongoClient(url,mongoParams)
+  
+  client.connect(err => {
         if (err) {
-            console.log(err.message)
-            throw new Error("failed to connect")
+        console.log(err.message)
+        throw new Error("failed to connect")
         }
-        let dbase = client.db(db_name)
-        console.log('db connected')
-
-            // Get the documents collection
-            var collect = dbase.collection('macroData');
-            // Create the index
-            collect.createIndex(
-              { release_name : "text" });
-            
-            // console.log(collect.getIndexes())
-            
-
-            collect.find({ $text: { $search: "Turnover" } }).toArray()
-            .then(res => {
-              console.log(res)
-            })
-            
+      
+  let dbase = client.db(db_name)
+  console.log('db connected')
+  
+  // Get the documents collection
+  var collect = dbase.collection('macroData');
+  // Create the index
+  collect.createIndex(
+            { release_name : "text" }
+  )
           
-       
-        
-    })
+
+  collect.find({ $text: { $search: term } }).toArray()
+          .then(res => {
+            console.log(res)
+          })
+
+
+
+})
+
+
+
+}
+
+
+searchCal("FOMC",mongoUrl)
